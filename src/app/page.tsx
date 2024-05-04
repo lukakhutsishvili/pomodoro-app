5ce6f925e1495759b12dec1e795fa1cdad101513
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-05-04 22:55:02 +0400

    wip: timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="px-6 kumbh-font">
+    <main className="px-6 kumbh-font grid">
       <div className="mt-8 flex justify-center">
-        <Image
-          src="/assets/logo.svg"
-          alt="Logo"
-          width={200}
-          height={100}
-          unoptimized={true}
-        />
+        <img src="/assets/logo.svg" alt="Logo" />
       </div>
       <div className="mt-[45px] h-[63px] p-2 flex justify-between rounded-[31px] bg-darkBlue">
         <div className="px-[18px] flex items-center bg-red-500 rounded-[26px]">
@@ -25,7 +19,12 @@ export default function Home() {
           <p className="text-[12px] font-bold">long break</p>
         </div>
       </div>
-      <div className="w-[300px] h-[300px]"></div>
+      <div className="w-[300px] h-[300px] rounded-[50%] bg justify-self-center mt-12 p-4 ">
+        <div className="p-[10px] bg-darkBlue h-full rounded-[50%]">
+          
+        </div>
+      </div>
+        <img className="justify-self-center mt-[79px]" src="/assets/icon-settings.svg"/>
     </main>
   );
 }
