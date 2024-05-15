3a8d5dce50893eee130c050cb10ba38f60c83446
Luka Khutsishvili <lukakhutsishvili777@gmail.com>
2024-05-15 19:37:05 +0400

    fix: timer functional

--- src/app/components/timer.tsx
+++ src/app/components/timer.tsx
@@ -3,119 +3,101 @@ import "react-circular-progressbar/dist/styles.css";
 import { allContext } from "../page";
 import { useEffect, useState } from "react";
 
-type timerType = {
+type TimerType = {
   pomodoro: number;
   shortBreak: number;
   longBreak: number;
 };
 
+type TimerMode = "pomodoro" | "shortBreak" | "longBreak";
+
 const Timer = () => {
   const { allInfo, name } = allContext();
-  const mode =
-    name === "pomodoro"
-      ? allInfo.pomodoro
-      : name === "long break"
-      ? allInfo.longBreak
-      : name === "short break"
-      ? allInfo.shortBreak
-      : 0;
 
-  const [seconds, setSeconds] = useState<{ [key: string]: number }>({
-    pomodoro: 0,
-    shortBreak: 0,
-    longBreak: 0,
-  });
-  const [minutes, setMinutes] = useState<{ [key: string]: number }>({
-    pomodoro: allInfo.pomodoro,
-    shortBreak: allInfo.shortBreak,
-    longBreak: allInfo.longBreak,
-  });
-
-  const formattedMinutes: { [key: string]: string } = {};
-  const formattedSeconds: { [key: string]: string } = {};
-  // Iterate over each key in minutes object and apply padStart
-  for (const key in minutes) {
-    formattedMinutes[key] = String(minutes[key]).padStart(2, "0");
-  }
-
-  // Iterate over each key in seconds object and apply padStart
-  for (const key in seconds) {
-    formattedSeconds[key] = String(seconds[key]).padStart(2, "0");
-  }
-
-  console.log(formattedMinutes);
   const bgColor =
     allInfo.color === "pomodoro"
       ? "#f87070"
       : allInfo.color === "lightBlue"
       ? "#70f3f8"
-      : allInfo.color === "lightBlue"
+      : allInfo.color === "purple"
       ? "#d881f8"
       : "";
 
+  const [pause, setPause] = useState({
+    pomodoro: false,
+    shortBreak: true,
+    longBreak: true,
+  });
 
+  const [seconds, setSeconds] = useState<TimerType>({
+    pomodoro: 0,
+    shortBreak: 0,
+    longBreak: 0,
+  });
+  const [minutes, setMinutes] = useState<TimerType>({
+    pomodoro: allInfo.pomodoro,
+    shortBreak: allInfo.shortBreak,
+    longBreak: allInfo.longBreak,
+  });
 
-  // useEffect(() => {
-  //   setMinutes(mode);
-  //   setSeconds(0);
-  // }, [name]);
+  const formattedTime = (time: number) => String(time).padStart(2, "0");
 
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     if (minutes === 0 && seconds === 0) {
-  //       clearInterval(timer);
-  //     } else if (seconds > 0) {
-  //       setSeconds(seconds - 1);
-  //     } else if (seconds === 0) {
-  //       if (minutes > 0) {
-  //         setMinutes(minutes - 1);
-  //         setSeconds(59);
-  //       }
-  //     }
-  //   }, 1000);
+  const getMode = (name: string): TimerMode => {
+    switch (name) {
+      case "pomodoro":
+        return "pomodoro";
+      case "short break":
+        return "shortBreak";
+      case "long break":
+        return "longBreak";
+      default:
+        return "pomodoro";
+    }
+  };
 
-  //   return () => clearInterval(timer);
-  // }, [minutes, seconds]);
+  const currentMode = getMode(name);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("Minutes:", minutes[currentMode]);
+      console.log("Seconds:", seconds[currentMode]);
+
+      if (minutes[currentMode] === 0 && seconds[currentMode] === 0) {
+        console.log("Timer Ended");
+        clearInterval(timer);
+      } else if (seconds[currentMode] > 0) {
+        console.log("Seconds Decreased");
+        setSeconds((prev) => ({
+          ...prev,
+          [currentMode]: prev[currentMode] - 1,
+        }));
+      } else if (minutes[currentMode] > 0 && seconds[currentMode] === 0) {
+        console.log("Reset");
+        setSeconds((prev) => ({ ...prev, [currentMode]: 59 }));
+        setMinutes((prev) => ({
+          ...prev,
+          [currentMode]: prev[currentMode] - 1,
+        }));
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [minutes, seconds]);
 
   return (
     <div className="w-[300px] h-[300px] rounded-[50%] bg justify-self-center mt-12 p-4 ">
       <div className="p-[10px] bg-darkBlue h-full rounded-[50%]">
         <CircularProgressbar
-          value={50}
-          text={`${formattedMinutes}:${formattedSeconds}`}
-          styles={{
-            // Customize the root svg element
-            root: {},
-            // Customize the path, i.e. the "completed progress"
-            path: {
-              // Path color
-              stroke: bgColor,
-              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-              strokeLinecap: "round",
-              // Customize transition animation
-              transition: "stroke-dashoffset 0.5s ease 0s",
-              // Rotate the path
-              transform: "",
-              transformOrigin: "center center",
-
-              strokeWidth: "5px",
-            },
-            // Customize the circle behind the path, i.e. the "total progress"
-            trail: {
-              display: "none",
-            },
-            // Customize the text
-            text: {
-              // Text color
-              fill: "#d7e0ff",
-              // Text size
-              fontSize: "16px",
-            },
-            // Customize background - only used when the `background` prop is true
-            background: {
-              fill: "#3e98c7",
-            },
-          }}
+          value={100}
+          text={`${formattedTime(minutes[currentMode])}:${formattedTime(
+            seconds[currentMode]
+          )} `}
+          styles={buildStyles({
+            pathColor: bgColor,
+            strokeLinecap: "round",
+            textColor: "#d7e0ff",
+            textSize: "16px",
+            trailColor: "transparent",
+          })}
         />
       </div>
     </div>
